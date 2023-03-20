33654b81d6e60108d9aff75172aafb59b6e80f59
jinheonyoon <yjinheon@gmail.com>
2023-03-20 20:33:36 +0900

    docker 수정

--- services/backend/src/auth/jwthandler.py
+++ services/backend/src/auth/jwthandler.py
@@ -7,7 +7,6 @@ from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
 from fastapi.security import OAuth2
 from jose import JWTError, jwt
 from tortoise.exceptions import DoesNotExist
-
 from src.schemas.token import TokenData
 from src.schemas.users import UserOutSchema
 from src.database.models import Users
@@ -17,6 +16,12 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 class OAuth2PasswordBearerWithCookie(OAuth2):
+    """
+    a class that inherits from OAuth2
+
+    - read the cookie sent from request
+    - ensures that the cookie is a bearer token
+    """
     def __init__(
             self,
             token_url: str,
@@ -29,3 +34,65 @@ class OAuth2PasswordBearerWithCookie(OAuth2):
         flows = OAuthFlowsModel(password={"tokenUrl": token_url,
                                             "scopes": scopes})
         super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request):
+        authorization: str = request.cookies.get("Authorization")
+        scheme, param = get_authorization_scheme_param(authorization)
+
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=401,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+
+security = OAuth2PasswordBearerWithCookie(token_url="/login")
+
+def create_access_token(data:dict,
+                        expires_delta: Optional[timedelta] = None):
+    """
+    create a jwt token from the username
+    :param data:
+    :param expires_delta:
+    :return:
+    """
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15) # default expire time
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(security)):
+    """
+    decode the token and validate the user
+    :param token:
+    :return:
+    """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    try:
+        user = await UserOutSchema.from_queryset_single(Users.get(username=token_data.username))
+    except DoesNotExist:
+        raise credentials_exception
+    return user
